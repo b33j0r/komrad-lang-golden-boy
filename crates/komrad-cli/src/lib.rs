79e97734b13048eb54a5e6d6cbfc4f5ab9abd1ff
Brian Jorgensen <brian.jorgensen@gmail.com>
2025-03-09 08:13:38 -0600

    clap

---
+++ crates/komrad-cli/src/lib.rs
@@ -0,0 +1,60 @@
+use clap::{Parser, Subcommand};
+use owo_colors::OwoColorize;
+use palette::{LinSrgb};
+use tracing::{info, trace};
+
+mod banner;
+
+#[derive(Clone, Debug, Parser)]
+#[command(name = "komrad", version, about = "Komrad CLI")]
+struct Args {
+    #[clap(subcommand)]
+    subcommand: Option<Subcommands>,
+
+
+    #[command(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
+}
+
+#[derive(Clone, Debug, Subcommand)]
+enum Subcommands {
+    /// Print a banner
+    Banner {
+        /// The text to print in the banner
+        text: String,
+    },
+}
+
+pub async fn main() {
+    let args = Args::parse();
+
+    // Initialize tracing
+    tracing_subscriber::fmt()
+        .with_max_level(args.verbose)
+        .with_target(false)
+        // .with_line_number(true)
+        // .with_file(true)
+        .without_time()
+        .with_ansi(true)
+        .with_level(true)
+        .init();
+
+    banner();
+
+    info!("{}", "Komrad CLI starting".bright_cyan());
+
+}
+
+fn banner() {
+    let text = "KOMRAD 0.1";
+
+    let stops = vec![
+        (0.0, LinSrgb::new(1.0, 0.0, 0.5)),
+        (0.3, LinSrgb::new(0.85, 0.5, 0.9)),
+        (0.7, LinSrgb::new(0.8, 0.7, 0.0)),
+        (1.0, LinSrgb::new(0.3, 0.7, 0.9)),
+    ];
+
+    let banner = banner::gradient_banner(text, &stops);
+    trace!("\n{}", banner);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use figlet_rs::FIGfont;
 
-fn main() {
-    let standard_font = FIGfont::standard().unwrap();
-    //let my_font = //
-    let figure = standard_font.convert("KoMRaD");
-    assert!(figure.is_some());
-    println!("{}", figure.unwrap());
+
+#[tokio::main]
+async fn main() {
+    komrad_cli::main().await;
 }
-
